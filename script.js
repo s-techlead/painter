706c3253a3f7738aa7264667565c10d6f7a885f6
s-techlead <sebastienschnd.techlead@gmail.com>
2022-02-10 15:00:00 +0100

    Capture mouse event on canvas and draw a circle

--- script.js
+++ script.js
@@ -3,15 +3,30 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 30;
+let x = 50;
+let y = 50;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
+    console.log(e);
+    const x = e.offsetX;
+    const y = e.offsetY;
 
+    drawCircle(x, y);
 });
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.stroke();
+    ctx.fill();
 }
 
-drawCircle(200, 200);
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     drawCircle(x++, y);
+
+//     requestAnimationFrame(draw);
+// }
+
+// draw();
